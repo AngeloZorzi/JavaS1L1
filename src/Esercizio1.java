32fc177f21213e7067be65a6b46657b44a9994dc
AngeloZorzi <astro.drg666@gmail.com>
2025-05-05 15:31:11 +0200

    E1

--- src/Esercizio1.java
+++ src/Esercizio1.java
@@ -1,10 +1,17 @@
 public class Esercizio1 {
     public static void main(String[] args) {
+
        int m = moltiplicazione(3,5);
         System.out.println(m);
 
+       String c = concatenare("Result -> ",m);
+        System.out.println(c);
     }
     public static int moltiplicazione (int a , int b ){
         return a*b;
     }
+    public static String concatenare (String testo, int numero){
+        return testo + numero;
+    }
+    
 }
