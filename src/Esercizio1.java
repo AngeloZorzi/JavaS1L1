5f70082faa8fd877bf4bca46802644986da4d2bc
AngeloZorzi <astro.drg666@gmail.com>
2025-05-05 14:54:56 +0200

    Initial commit

---
+++ src/Esercizio1.java
@@ -0,0 +1,10 @@
+public class Esercizio1 {
+    public static void main(String[] args) {
+       int m = moltiplicazione(3,5);
+        System.out.println(m);
+
+    }
+    public static int moltiplicazione (int a , int b ){
+        return a*b;
+    }
+}
