ff6e5a0cdb8e37b126809ceae4d03e13a914fc10
AngeloZorzi <astro.drg666@gmail.com>
2025-05-05 16:41:54 +0200

    Update Esercizio1.java

--- src/Esercizio1.java
+++ src/Esercizio1.java
@@ -15,6 +15,20 @@ public class Esercizio1 {
 
        String c = concatenare("Result -> ",m);
         System.out.println(c);
+
+        String[] original = { "a", "b", "c", "d", "e" };
+        String nuova = "f";
+
+        String[] result = changes(original, nuova);
+
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+        System.out.println(result[2]);
+        System.out.println(result[3]);
+        System.out.println(result[4]);
+        System.out.println(result[5]);
+
+
     }
     public static int moltiplicazione (int a , int b ){
         return a*b;
@@ -22,6 +36,23 @@ public class Esercizio1 {
     public static String concatenare (String testo, int numero){
         return testo + numero;
     }
-    
+    public static String[] changes (String[] array5, String nuova) {
+        String[] array6 = new String[6];
+
+
+        array6[0] = array5[0];
+        array6[1] = array5[1];
+
+
+        array6[2] = nuova;
+
+
+        array6[3] = array5[2];
+        array6[4] = array5[3];
+        array6[5] = array5[4];
+
+        return array6;
+    }
+
 
 }
